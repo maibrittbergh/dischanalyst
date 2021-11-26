021ea289cc0729ee708f870461f879c8fcc4cf07
Mai-Britt Berghoefer <maibrittberghoefer@gmx.de>
2021-11-26 11:09:51 +0100

    First function in Package: - File: Generate Skeleton by ROxygen2 --> have exprt, so function can be used and is written in NAMESPACE . Functions you want users of your package to be able to use - delete NAMESPACE and content of man-Folder - clean and rebuild

---
+++ R/grdc_readr.R
@@ -0,0 +1,51 @@
+
+
+#' GRDCC River Dataset Reader.
+#'
+#'
+#'
+#' @param data Dataframe ;  a Dataframe including columns: "river" (rivername as character), "station" (rivername as character) and "grdc_no" (grdc number as double)
+#' @param rivername character ; name of the demanded river. Must equal name in column "river" in the Dataframe
+#' @param path character; Pathway of your computer where the GRDC dataset is saved.
+#'
+#' @return  dataset of the demanded river
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#' mosel=grdc_readr(data,"MOSELLE RIVER","/Users/username/Desktop/worfolder/datafolder/grdc_03_2021/grdc_disc/" )
+#' }
+#'
+#'
+#'
+#'
+#'
+#'
+grdc_readr=function(data, rivername, path ){
+  data_fluss= data[which(data$river==rivername),]
+  grdc_no=data_fluss$grdc_no
+  l=length(grdc_no)
+  read=c(1:l)
+  for (i in 1:l){
+
+    grdc_numb=grdc_no[i]
+    grdc_numb=as.character(grdc_numb)
+    read[i]=paste(path,grdc_numb,"_Q_Day.Cmd.txt")
+  }
+  read=sub(" ","", read)
+  read=sub(" _","_", read)
+  name= vector(mode = "list", length = l)
+  for (i in 1:l){
+    Tabelle=read.table(read[i], header=T, sep=";", dec=".", na.strings = c())[-2] #-999 als NA Value
+    Tabelle$YYYY.MM.DD=as.Date(Tabelle$YYYY.MM.DD)
+    Tabelle$Value[(which(Tabelle$Value<0))] = 0
+    name[[i]]=Tabelle #hours,minutes rausgeschmissen
+  }
+
+  for (i in 1:l){
+    names(name)[[i]]= data_fluss$station[i]
+  }
+
+  return(name)
+}
