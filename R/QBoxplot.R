0f50a6ad2daaf61013894adfe8679d90a7e16a1a
Mai-Britt Berghoefer <maibrittberghoefer@gmx.de>
2021-11-29 15:17:36 +0100

    Reset functions. As Error occured.

--- R/QBoxplot.R
+++ R/QBoxplot.R
@@ -1,6 +1,6 @@
 #' Boxplot of Discharge Data
 #'
-#' @description  Desciptive Statistics. Boxplot of Time Series of Discharge at specific station. Including all measurements in list.
+#'@description  Desciptive Statistics. Boxplot of Time Series of Discharge at specific station. Including all measurements in list.
 #'
 #' @param data River from GRDC - Dataset. Output of grdc-readr function. Type: list; list entries: measurement stations. For every Station: Date since begin of Measurements (as character) and Value (as numeric).
 #' @param station as character. Name of the Station e.g. "COCHEM" - must be named equally like list entry in data.
@@ -8,13 +8,14 @@
 #'
 #' @return Boxplot Graphic of Discharge time series.
 #' @export
+#'
 #' @import ggplot2
 #'
 #' @examples
-#' \dontrun{QBoxplot(mosel, "Mosel", "COCHEM")}
+#' \dontrun{ QBoxplot(mosel, "Mosel", "COCHEM")}
 
 QBoxplot=function(data, Name, station){
   titl=paste("Boxplot of", Name,",", station )
-  plot=ggplot(data[[station]])+geom_boxplot(aes(y=Value, color="red"))+labs(title=titl, subtitle="From GRDC-Datensatz des BfG")+ theme(legend.position="none")
+  plot=ggplot(data[[station]])+geom_boxplot(aes(y=Value, color="red"))+labs(title=titl, subtitle="Aus GRDC-Datensatz des BfG")+theme(legend.position="none")
   return(plot)
 }
