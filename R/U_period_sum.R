760a0ed8e6d031d39d91e7e15b557195e75eddf0
Mai-Britt Berghoefer <maibrittberghoefer@gmx.de>
2021-12-01 14:00:55 +0100

    included Function: U_period_sum

---
+++ R/U_period_sum.R
@@ -0,0 +1,22 @@
+
+#' Sum of Days under U within all measurements
+#'
+#'
+#' @param U numeric; Limit Value. Function returns sum of  days  Value U within all measurements.
+#' @param station haracter; Name of the Station e.g. "COCHEM" - must be named equally like list entry in data.
+#' @param data list; River from GRDC - Dataset. Output of grdc-readr function. list entries: measurement stations. For every Station: Date since begin of Measurements (as character) and Value (as numeric).
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{ U_period_sum(88, "TRIER UP", mosel)}
+
+U_period_sum=function(U, station, data){
+  nbr=which(names(data)==station)
+  Val=data[[nbr]][,2]
+  uU=which(Val<U)
+  l=length(uU)
+  if (all(Val>U)) {return(paste("No days"))}
+  else {return(paste(l, "days were under given Value"))}
+}
