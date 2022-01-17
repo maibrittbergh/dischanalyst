c7c2441de55380b5639600a97a072334a6f1259c
Mai-Britt Berghoefer <maibrittberghoefer@gmx.de>
2022-01-17 14:38:52 +0100

    Introducing grdc_list function

---
+++ R/grdc_list.R
@@ -0,0 +1,41 @@
+#'GRDC List
+#'
+#'@description Function returns List. Each entry contains Discharge Measurements as well as the corresponding date of a specific station. The Number of the stations depends on the metadata.
+#'It may take some time to load the GRDC-List.
+#'
+#'
+#' @param metadata Data Frame. Overview of GRDC-Dataset.  The metadata can be created by \link[dischanalyst]{metadata_grdc} function
+#' @param path character; pathway to grdc_discharge folder on computer
+#'
+#' @return List
+#' @export
+#'
+#' @examples
+#' grdc_list(metadata_germany, "/Users/username/Desktop/folderone/datafolder/grdc_03_2021/grdc_disc/")
+#'
+#'
+#'
+grdc_list=function(metadata, path){
+
+  length=nrow(metadata)
+  grdc_list=vector(mode = "list", length = length)
+
+
+
+  for ( i in 1:length){
+
+    data=grdc_readr(metadata,   metadata$river[i]    , path)
+    station=metadata$station[i]
+    nbr=which(names(data)== station)
+    val=data[[nbr]]
+
+
+
+    grdc_list[[i]]=val
+    names(grdc_list)=metadata$station
+
+  }
+
+
+  return(grdc_list)
+}
